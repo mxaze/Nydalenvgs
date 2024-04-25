358d68ef3fe366c206d64dfd8cb6c6907e7a59b9
mxaze <narcie.nar@gmail.com>
2024-04-25 10:35:09 +0200

    uhm?

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { Prisma } = require('@prisma/client');
 const express = require('express');
 
 const app = express();
@@ -5,6 +6,18 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Working');
 });
+
+app.get('/', (req, res) => {
+  const allUsers = Prisma.user.findMany();
+  res.json(allUsers);
+});
+
+app.post('/', (req, res) => {
+  const newUser =  Prisma.user.create({ data: req.body});
+  res.json(newUser);
+});
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
