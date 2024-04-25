dc308bb232a3d873a00051fb87a0fa697b134163
mxaze <narcie.nar@gmail.com>
2024-04-25 13:15:40 +0200

    funker ENDELIG

--- app.js
+++ app.js
@@ -1,61 +1,112 @@
-const { PrismaClient } = require('@prisma/client');
-const express = require('express');
+const { PrismaClient, Role } = require("@prisma/client");
+const express = require("express");
 const app = express();
 const prisma = new PrismaClient();
+const cookieparser = require("cookie-parser");
+const crypto = require("crypto");
+const { register } = require("module");
 
-app.get('/', (req, res) => {
-  res.send('Working');
+app.use(express.json());
+app.use(cookieparser());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/register.html");
 });
 
-app.get('/', async (_, res) => {
-  const allUsers = await prisma.user.findMany();
-  res.json(allUsers);
-});
+app.use(async (req, res, next) => {
+  const path = req.path;
+  const token = req.cookies.token;
 
-app.post('/', async (req, res) => {
-  const newUser = await prisma.user.create({ data: req.body });
-  res.json(newUser);
-});
-
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Bent',
-      email: 'Bent@gmail.com',
+  if (!token) return res.redirect("/");
+  const user = await prisma.users.findFirst({
+    where: {
+      token: token,
     },
-  })
-  console.log(user)
+  });
+  if (!user) return res.redirect("/");
+
+  if (adminPaths.includes(path) && !Role.ADMIN) {
+    return res.redirect("/welcome");
+  }
+
+  next(); // if user is found, continue
+});
+
+
+async function createUser() {
+  const user = await prisma.users.create({
+    data: {
+      firstname: "test",
+      lastname: "testing",
+      mail: "test@test.com",
+      password: crypto.createHash("123123").update("Passord01").digest("hex"),
+      role: Role.STUDENT,
+    },
+  });
+
+  console.log(user.firstname + "" + "has been created");
+
+  return user;
 }
 
-async function main() {
-  await prisma.user.create({
+// function to create an admin
+async function createAdmin() {
+  const admin = await prisma.users.create({
     data: {
-      name: 'Bent',
-      email: 'Bent@prisma.io',
-      posts: {
-        create: { title: 'teach' },
-      },
-      profile: {
-        create: { bio: 'I like drift' },
-      },
+      firstname: "admin",
+      lastname: "admin",
+      mail: "admin@test.com",
+      password: crypto.createHash("123123").update("Passord01").digest("hex"),
+      role: Role.ADMIN,
     },
-  })
+  });
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-  console.dir(allUsers, { depth: null })
+  console.log(`${admin.firstname} has been created`);
+
+  return admin;
 }
 
-main().catch((error) => {
-  console.error(error);
-}).finally(() => {
-  prisma.$disconnect();
+// function to create a student
+app.post("/login", async (req, res) => {
+  const { mail, password } = req.body;
+
+  const userData = await prisma.users.findFirst({
+    where: {
+      mail: mail,
+      password: crypto.createHash("123123").update(password).digest("hex"),
+    },
+  });
+
+  if (userData) {
+    switch (userData.role === Role.ADMIN) {
+      case true:
+        res.cookie("token", userData.token);
+        res.redirect("/admin");
+        break;
+      case false:
+        res.cookie("token", userData.token);
+        res.redirect("/welcome");
+        break;
+    }
+  } else {
+    res.redirect("/");
+  }
 });
+
+const apiRoutes = {
+  login: (req, res) => {
+    res.sendFile(__dirname + "/public/login.html");
+  },
+  register: (req, res) => {
+    res.sendFile(__dirname + "/public/register.html");
+  },
+};
+
+
+app.post("/api/registerUser", apiRoutes.register);
+
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
