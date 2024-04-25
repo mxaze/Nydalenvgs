c67acb84747888738bfb3cc9f0898050c43a1707
mxaze <narcie.nar@gmail.com>
2024-04-25 11:42:09 +0200

    gorgi fix

--- app.js
+++ app.js
@@ -1,23 +1,60 @@
-const { Prisma } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 const express = require('express');
-
 const app = express();
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
   res.send('Working');
 });
 
-app.get('/', (req, res) => {
-  const allUsers = Prisma.user.findMany();
+app.get('/', async (_, res) => {
+  const allUsers = await prisma.user.findMany();
   res.json(allUsers);
 });
 
-app.post('/', (req, res) => {
-  const newUser =  Prisma.user.create({ data: req.body});
+app.post('/', async (req, res) => {
+  const newUser = await prisma.user.create({ data: req.body });
   res.json(newUser);
 });
 
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Bent',
+      email: 'Bent@gmail.com',
+    },
+  })
+  console.log(user)
+}
 
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: 'Bent',
+      email: 'Bent@prisma.io',
+      posts: {
+        create: { title: 'teach' },
+      },
+      profile: {
+        create: { bio: 'I like drift' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main().catch((error) => {
+  console.error(error);
+}).finally(() => {
+  prisma.$disconnect();
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
