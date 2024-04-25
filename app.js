313745d478dbeffaa576267404f39ec47b2e94f2
mxaze <narcie.nar@gmail.com>
2024-04-25 14:33:36 +0200

    leo fix!

--- app.js
+++ app.js
@@ -4,16 +4,41 @@ const app = express();
 const prisma = new PrismaClient();
 const cookieparser = require("cookie-parser");
 const crypto = require("crypto");
-const { register } = require("module");
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(cookieparser());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/login.html");
 });
 
+app.post("/api/login", async (req, res) => {
+  const { mail, password } = req.body;
+
+  const userData = await prisma.users.findFirst({
+    where: {
+      mail: mail,
+      password: sha256(password),
+    },
+  });
+  if (userData) {
+    switch (userData.role === Role.ADMIN) {
+      case true:
+        res.cookie("token", userData.token);
+        res.redirect("/admin");
+        break;
+      case false:
+        res.cookie("token", userData.token);
+        res.redirect("/welcome");
+        break;
+    }
+  } else {
+    res.redirect("/");
+  }
+});
+/* 
 app.use(async (req, res, next) => {
   const path = req.path;
   const token = req.cookies.token;
@@ -32,39 +57,14 @@ app.use(async (req, res, next) => {
 
   next(); // if user is found, continue
 });
-
-app.post("/login", async (req, res) => {
-  const { mail, password } = req.body;
-
-  const userData = await prisma.users.findFirst({
-    where: {
-      mail: mail,
-      password: crypto.createHash("sha256").update(password).digest("hex"),
-    },
-  });
-  if (userData) {
-    switch (userData.role === Role.ADMIN) {
-      case true:
-        res.cookie("token", userData.token);
-        res.redirect("/admin");
-        break;
-      case false:
-        res.cookie("token", userData.token);
-        res.redirect("/welcome");
-        break;
-    }
-  } else {
-    res.redirect("/");
-  }
-});
-
+ */
 function createUser() {
   const user = prisma.users.create({
     data: {
       firstname: "test",
       lastname: "testing",
       mail: "test@test.com",
-      password: crypto.createHash("123123").update("Passord01").digest("hex"),
+      password: sha256("Passord01"),
       role: Role.STUDENT,
     },
   });
@@ -72,47 +72,7 @@ function createUser() {
   console.log(user.firstname + "" + "has been created");
 }
 
-// function to create an admin
-function createAdmin() {
-  const admin = prisma.users.create({
-    data: {
-      firstname: "admin",
-      lastname: "admin",
-      mail: "admin@test.com",
-      password: crypto.createHash("123123").update("Passord01").digest("hex"),
-      role: Role.ADMIN,
-    },
-  });
-
-  console.log(`${admin.firstname} has been created`);
-}
-
 // function to create a student
-app.post("/login", async (req, res) => {
-  const { mail, password } = req.body;
-
-  const userData = await prisma.users.findFirst({
-    where: {
-      mail: mail,
-      password: crypto.createHash("123123").update(password).digest("hex"),
-    },
-  });
-
-  if (userData) {
-    switch (userData.role === Role.ADMIN) {
-      case true:
-        res.cookie("token", userData.token);
-        res.redirect("/admin");
-        break;
-      case false:
-        res.cookie("token", userData.token);
-        res.redirect("/welcome");
-        break;
-    }
-  } else {
-    res.redirect("/");
-  }
-});
 
 async function createAdmin() {
   const admin = await prisma.users.create({
@@ -120,7 +80,7 @@ async function createAdmin() {
       firstname: "admin",
       lastname: "admin",
       mail: "admin@test.com",
-      password: crypto.createHash("sha256").update("Passord01").digest("hex"),
+      password: sha256("Passord01"),
       role: Role.ADMIN,
     },
   });
@@ -147,10 +107,14 @@ const pageroutes = {
   },
 };
 
-
+ 
 app.post("/api/registerUser", apiRoutes.register);
 
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+async function sha256(message) {
+  return crypto.createHash("sha256").update(message).digest("hex");
+}
